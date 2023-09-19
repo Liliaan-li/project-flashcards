3b01a00577bd73b37c0cda0c0dd53d516d8e442e
Liliaan-li <liliya.kop2003@gmail.com>
2023-09-19 20:46:09 +0300

    bugfix/eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import {Typography} from "@/components/ui/typography/typography";
 import className from './components/ui/typography/typography.module.scss'
-export function App() {
-  return <div>
-    <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
 
+import { Typography } from '@/components/ui/typography/typography'
+
+export function App() {
+  return (
+    <div>
+      <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
     </div>
+  )
 }
