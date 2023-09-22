c59d058dad95e8c49a2c68f7414d28e65afa7b10
Liliaan-li <liliya.kop2003@gmail.com>
2023-09-22 15:05:52 +0300

    features/added new parameter 'color'

--- src/icons/IconWrapper.tsx
+++ src/icons/IconWrapper.tsx
@@ -2,15 +2,18 @@ import * as React from 'react'
 
 export type IconProps = {
   size?: number
+  color?: string
   svgProps?: React.SVGProps<SVGSVGElement>
 } & Omit<React.HTMLProps<HTMLSpanElement>, 'color' | 'size'>
 
 export const IconWrapper: React.FC<{ icon: React.ReactNode } & IconProps> = ({
   icon,
   size: sizeProp,
+  color: colorProp,
   ...restProps
 }) => {
   const size = sizeProp ? `${sizeProp}px` : '20px'
+  const color = colorProp ? colorProp : 'currentColor'
 
   return (
     <span
@@ -18,6 +21,7 @@ export const IconWrapper: React.FC<{ icon: React.ReactNode } & IconProps> = ({
       style={{
         width: size,
         height: size,
+        color: color,
         display: 'inline-flex',
         fontSize: 'inherit',
       }}
