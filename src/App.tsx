2f0487552b9d9a95aa72389e7f0104393b7d5aa1
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-09-26 18:14:26 +0300

    checkbox-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
+
+import { Checkbox } from './components/ui/checkbox/checkbox'
 import className from './components/ui/typography/typography.module.scss'
 
 // eslint-disable-next-line import/no-unresolved
 import { Typography } from '@/components/ui/typography/typography'
 
 export function App() {
+  const [bool, setBool] = useState<boolean>(false)
+
+  function onChange(checked: boolean): void {
+    setBool(checked)
+  }
+
   return (
     <div>
       <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
+      <Checkbox checked={bool} onChange={onChange} />
     </div>
   )
 }
