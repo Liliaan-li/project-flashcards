965c302074a730825808ccc96bd5259fa23c3f40
Liliaan-li <liliya.kop2003@gmail.com>
2023-09-30 17:24:50 +0300

    features/added @radix-ui/react-tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 
-import { Checkbox } from './components/ui/checkbox/checkbox'
-import className from './components/ui/typography/typography.module.scss'
-
-// eslint-disable-next-line import/no-unresolved
-import { Typography } from '@/components/ui/typography/typography'
+import { Checkbox } from '@/components/ui/checkbox'
+import { TabContent, Tabs } from '@/components/ui/tab-switcher'
 
 export function App() {
   const [bool, setBool] = useState<boolean>(false)
@@ -15,7 +12,21 @@ export function App() {
 
   return (
     <div>
-      <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
+      <Tabs
+        tabs={[
+          { value: 'sprints', title: 'Switch1' },
+          { value: 'weeks', title: 'Switch2' },
+          { value: 'subjects', title: 'Switch3', disabled: true },
+        ]}
+        defaultValue={'sprints'}
+        label={'Show packs cards'}
+      >
+        <Fragment key=".0">
+          <TabContent value="sprints">Switch1</TabContent>
+          <TabContent value="weeks">Switch2</TabContent>
+          <TabContent value="subjects">Switch3</TabContent>
+        </Fragment>
+      </Tabs>
       <Checkbox checked={bool} onChange={onChange} />
     </div>
   )
