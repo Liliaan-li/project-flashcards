c5fae209613954920e00cdf1710a27a4f5482071
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-09-30 13:10:03 +0300

    feat: add forgot-password and forgot-password.stories.tsx

---
+++ src/stories/components/auth/forgot-password/forgot-password.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ForgotPassword } from '../../../../components/auth/forgot/forgot-password'
+
+const meta = {
+  title: 'Auth/Forgot password',
+  component: ForgotPassword,
+  tags: ['autodocs'],
+} satisfies Meta<typeof ForgotPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    onSubmit: data => console.info(data),
+  },
+}
