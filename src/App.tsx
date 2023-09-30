a6273982dd078d2849ce8be61ec7717978684e70
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-09-30 12:46:30 +0300

    controlchecbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
-import { Checkbox } from './components/ui/checkbox/checkbox'
+import { ControlledCheckbox } from './components/ui/controlled/conrolled-checkbox/controlled-checkbox'
+import { Typography } from './components/ui/typography/typography'
 import className from './components/ui/typography/typography.module.scss'
 
-// eslint-disable-next-line import/no-unresolved
-import { Typography } from '@/components/ui/typography/typography'
-
 export function App() {
-  const [bool, setBool] = useState<boolean>(false)
-
-  function onChange(checked: boolean): void {
-    setBool(checked)
-  }
+  const { control } = useForm()
 
   return (
     <div>
       <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
-      <Checkbox checked={bool} onChange={onChange} />
+      <ControlledCheckbox
+        control={control}
+        name={'rememberMe'}
+        onChange={function (checked: boolean): void {
+          throw new Error('Function not implemented.')
+        }}
+      />
     </div>
   )
 }
---
+++ src/components/ui/controlled/conrolled-checkbox/controlled-checkbox.tsx
@@ -0,0 +1,19 @@
+import { useController } from 'react-hook-form'
+
+import { Checkbox, CheckboxProps } from '../../checkbox'
+
+export type ControlledCheckboxProps = {
+  control: any
+  name: string
+} & Omit<CheckboxProps, 'checked' | 'onValueChange'>
+
+export const ControlledCheckbox = ({ control, name, ...rest }: ControlledCheckboxProps) => {
+  const {
+    field: { onChange, value },
+  } = useController({
+    name,
+    control,
+  })
+
+  return <Checkbox {...rest} checked={value} onChange={onChange} id={name} />
+}
