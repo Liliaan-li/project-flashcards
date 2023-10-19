16935a0403ff48f32ce3b07ea640f0a8d7db7910
Liliaan-li <liliya.kop2003@gmail.com>
2023-10-19 19:16:23 +0300

    features/added header.tsx and header.module.scss, also dropdown and typography were changed a little bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
-import { useForm } from 'react-hook-form'
-
-import { ControlledCheckbox } from './components/ui/controlled/conrolled-checkbox/controlled-checkbox'
-import { Typography } from './components/ui/typography/typography'
-import className from './components/ui/typography/typography.module.scss'
-
-import { useState, Fragment } from 'react'
-
-import { Checkbox } from '@/components/ui/checkbox'
-import { TabContent, Tabs } from '@/components/ui/tab-switcher'
+import { Header } from '@/components/ui/header/header.tsx'
 
 export function App() {
-  const { control } = useForm()
-
   return (
-    <div>
-      <Typography.Link2 className={className.link2}>Hi</Typography.Link2>
-      <ControlledCheckbox
-        control={control}
-        name={'rememberMe'}
-        onChange={function (checked: boolean): void {
-          throw new Error('Function not implemented.')
-        }}
-      />
-      <Tabs
-        tabs={[
-          { value: 'sprints', title: 'Switch1' },
-          { value: 'weeks', title: 'Switch2' },
-          { value: 'subjects', title: 'Switch3', disabled: true },
-        ]}
-        defaultValue={'sprints'}
-        label={'Show packs cards'}
-      >
-        <Fragment key=".0">
-          <TabContent value="sprints">Switch1</TabContent>
-          <TabContent value="weeks">Switch2</TabContent>
-          <TabContent value="subjects">Switch3</TabContent>
-        </Fragment>
-      </Tabs>
-      <Checkbox checked={bool} onChange={onChange} />
-    </div>
+    <Header
+      isAuth={true}
+      userInfo={{
+        name: 'Buba',
+        avatar:
+          'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
+        email: 'ioji@gmaikjjjjjjjjjjjjjjjjjjl.com',
+      }}
+    />
   )
 }
---
+++ src/assets/icons/components/logo/Logo.tsx
@@ -0,0 +1,5 @@
+import LogoImg from './Logo.svg'
+
+export const Logo = () => {
+  return <img src={LogoImg} alt={'logo img'} style={{ width: 157, height: 36 }} />
+}
