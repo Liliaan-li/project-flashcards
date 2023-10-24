6aa98e5e576ac0208408db80a5e9190900a09799
Liliaan-li <liliya.kop2003@gmail.com>
2023-10-24 17:31:02 +0300

    features/added table, icons and changed styles

--- src/router.tsx
+++ src/router.tsx
@@ -6,9 +6,9 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
-import { Button } from '@/components/ui/button'
-import { Table } from '@/components/ui/table'
-import { useCreateDeckMutation, useGetDecksQuery } from '@/services/decks/decks.service.ts'
+import { Header } from '@/components/ui/header'
+import { DecksPage } from '@/pages/decks-page/decks-page.tsx'
+import { useGetDecksQuery } from '@/services/decks/decks.service.ts'
 
 const publicRoutes: RouteObject[] = [
   {
@@ -20,7 +20,20 @@ const publicRoutes: RouteObject[] = [
 const privateRoutes: RouteObject[] = [
   {
     path: '/',
-    element: <Component />,
+    element: (
+      <>
+        <Header
+          isAuth={true}
+          userInfo={{
+            name: 'Buba',
+            avatar:
+              'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
+            email: 'ioji@gmaikjjjjjjjjjjjjjjjjjjl.com',
+          }}
+        />
+        <DecksPage />
+      </>
+    ),
   },
 ]
 
@@ -47,44 +60,44 @@ function PrivateRoutes() {
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
-
-function Component() {
-  const { data } = useGetDecksQuery()
-  const [createDeck, { isLoading: isDeckBeingCreated }] = useCreateDeckMutation()
-
-  return (
-    <>
-      <Button
-        onClick={() => {
-          createDeck({ name: 'new deck1' })
-        }}
-        style={{ width: '180px' }}
-      >
-        Create Deck
-      </Button>
-      {isDeckBeingCreated && <div>Creating deck....</div>}
-      <Table.Root>
-        <Table.Head>
-          <Table.Row>
-            <Table.HeadCell>Name</Table.HeadCell>
-            <Table.HeadCell>Cards</Table.HeadCell>
-            <Table.HeadCell>Last Updated</Table.HeadCell>
-            <Table.HeadCell>Created by</Table.HeadCell>
-            <Table.HeadCell></Table.HeadCell>
-          </Table.Row>
-        </Table.Head>
-        <Table.Body>
-          {data?.items.map(item => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>{item.cardsCount}</Table.Cell>
-              <Table.Cell>{new Date(item.updated).toLocaleDateString()}</Table.Cell>
-              <Table.Cell>{item.author.name}</Table.Cell>
-              <Table.Cell>icons...</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </>
-  )
-}
+//просто чтоб было
+// function Component() {
+//   const { data } = useGetDecksQuery()
+//   const [createDeck, { isLoading: isDeckBeingCreated }] = useCreateDeckMutation()
+//
+//   return (
+//     <>
+//       <Button
+//         onClick={() => {
+//           createDeck({ name: 'new deck1' })
+//         }}
+//         style={{ width: '180px' }}
+//       >
+//         Create Deck
+//       </Button>
+//       {isDeckBeingCreated && <div>Creating deck....</div>}
+//       <Table.Root>
+//         <Table.Head>
+//           <Table.Row>
+//             <Table.HeadCell>Name</Table.HeadCell>
+//             <Table.HeadCell>Cards</Table.HeadCell>
+//             <Table.HeadCell>Last Updated</Table.HeadCell>
+//             <Table.HeadCell>Created by</Table.HeadCell>
+//             <Table.HeadCell></Table.HeadCell>
+//           </Table.Row>
+//         </Table.Head>
+//         <Table.Body>
+//           {data?.items.map(item => (
+//             <Table.Row key={item.id}>
+//               <Table.Cell>{item.name}</Table.Cell>
+//               <Table.Cell>{item.cardsCount}</Table.Cell>
+//               <Table.Cell>{new Date(item.updated).toLocaleDateString()}</Table.Cell>
+//               <Table.Cell>{item.author.name}</Table.Cell>
+//               <Table.Cell>icons...</Table.Cell>
+//             </Table.Row>
+//           ))}
+//         </Table.Body>
+//       </Table.Root>
+//     </>
+//   )
+// }
