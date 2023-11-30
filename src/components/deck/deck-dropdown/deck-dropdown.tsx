09f8f53da29dfa7643c39280515900815f21df14
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-12-01 00:12:28 +0300

    feat: add add-deck and deck-dropdown, decks-edit, decks-panel

---
+++ src/components/deck/deck-dropdown/deck-dropdown.tsx
@@ -0,0 +1,74 @@
+import { FC } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import s from './deck-dropdown.module.scss'
+
+import { DeleteOutlined } from '@/assets/icons/components/DeleteForever/DeleteOutlined.tsx'
+import Edit from '@/assets/icons/components/edit/edit.tsx'
+import More from '@/assets/icons/components/More/More.tsx'
+import { Play } from '@/assets/icons/components/play/Play.tsx'
+import { Button } from '@/components/ui/button'
+import { Dropdown, ToolbarItemWithIcon } from '@/components/ui/dropdown/dropdown.tsx'
+import { Typography } from '@/components/ui/typography'
+import { Deck } from '@/services/decks'
+
+type DeckDropdownProps = {
+  data: Deck
+  isOwner: boolean
+  openCreateModal: () => void
+  setDeckDeleteId: (value: string) => void
+  setDeckEditId: (value: string) => void
+}
+
+export const DeckDropdown: FC<DeckDropdownProps> = ({
+  data,
+  isOwner,
+  openCreateModal,
+  setDeckDeleteId,
+  setDeckEditId,
+}) => {
+  const navigate = useNavigate()
+
+  return (
+    <div className={s.container}>
+      <Typography.LARGE>{data.name}</Typography.LARGE>
+      {isOwner && !!data.cardsCount && (
+        <div className={s.header}>
+          <div className={s.drop}>
+            <Dropdown
+              trigger={
+                <div className={s.more}>
+                  <More size={25} />
+                </div>
+              }
+            >
+              <div>
+                <ToolbarItemWithIcon
+                  icon={<Play color={'var(--color-light-100)'} />}
+                  text="Learn"
+                  onSelect={() => navigate(`/decks/${data.id}/learn`)}
+                />
+                <ToolbarItemWithIcon
+                  icon={<Edit color={'var(--color-light-100)'} />}
+                  text="Edit"
+                  onSelect={() => setDeckEditId(data.id)}
+                />
+                <ToolbarItemWithIcon
+                  icon={<DeleteOutlined color={'var(--color-light-100)'} />}
+                  text="Delete"
+                  onSelect={() => setDeckDeleteId(data.id)}
+                />
+              </div>
+            </Dropdown>
+          </div>
+          <div>
+            <Button onClick={openCreateModal}>
+              <Typography.Subtitle2>Add New Card</Typography.Subtitle2>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ src/components/decks/add-deck/add-deck.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import s from './add-deck.module.scss'
+
+import { ImageCover } from '@/assets/icons/components/image-cover/image-cover.tsx'
+import { UploadImage } from '@/components/cards/add-card/upload-image/upload-image.tsx'
+import { ControlledTextField } from '@/components/controlled'
+import { Button } from '@/components/ui/button'
+import { ControlledCheckbox } from '@/components/ui/controlled/conrolled-checkbox'
+import { Modal, ModalProps } from '@/components/ui/modal'
+import { Typography } from '@/components/ui/typography'
+
+export type DialogProps = ModalProps & {
+  onCancel?: () => void
+}
+
+type Props = Pick<DialogProps, 'onCancel' | 'onOpenChange' | 'open'> & {
+  onSubmit: (data: FormData) => void
+  name?: string
+  isPrivate?: boolean
+  image?: string | null
+}
+export const AddDeck = ({ onCancel, onSubmit, image, isPrivate, name, ...dialogProps }: Props) => {
+  const { control, handleSubmit, reset } = useDeckForm({
+    name: name || '',
+    isPrivate: isPrivate || false,
+  })
+
+  const [cover, setCover] = useState<File | null>(null)
+
+  const imageUrl = cover ? URL.createObjectURL(cover) : image
+
+  const onSubmitHandler = handleSubmit((data: FormValues) => {
+    const formData = new FormData()
+
+    formData.append('name', data.name)
+    formData.append('isPrivate', `${data.isPrivate}`)
+    cover && formData.append('cover', cover)
+
+    onSubmit(formData)
+
+    dialogProps.onOpenChange?.(false)
+    reset()
+  })
+
+  const handleCancel = () => {
+    reset()
+    onCancel?.()
+  }
+
+  const onLoadCover = (data: File) => {
+    setCover(data)
+  }
+
+  return (
+    <Modal {...dialogProps} title="Create new deck">
+      <form className={s.content} onSubmit={onSubmitHandler}>
+        {imageUrl && (
+          <div className={s.image}>
+            <img src={imageUrl} alt="Pack cover" />
+          </div>
+        )}
+        <UploadImage className={s.uploader} onLoadCover={onLoadCover}>
+          <Button type="button" variant="secondary">
+            <ImageCover />
+            <Typography.Subtitle2>Cover</Typography.Subtitle2>
+          </Button>
+        </UploadImage>
+        <ControlledTextField control={control} label="Name Pack" name="name" />
+        <ControlledCheckbox
+          onChange={() => {}}
+          control={control}
+          label="Private"
+          name="isPrivate"
+        />
+        <div className={s.button}>
+          <Button variant="secondary" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button type="submit">Add New Pack</Button>
+        </div>
+      </form>
+    </Modal>
+  )
+}
+
+const newDeckSchema = z.object({
+  name: z.string(),
+  isPrivate: z.boolean().default(false),
+})
+
+type FormValues = z.infer<typeof newDeckSchema>
+
+export const useDeckForm = (defaultValues: FormValues) => {
+  return useForm<FormValues>({
+    resolver: zodResolver(newDeckSchema),
+    defaultValues,
+  })
+}
