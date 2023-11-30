b174343f250a33ad62d487b8931c8ecd00b42f74
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-12-01 00:17:23 +0300

    feat: add cardsSlice, decks-page and learn-page in router

--- src/services/store.ts
+++ src/services/store.ts
@@ -3,12 +3,14 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 import { baseApi } from '@/services/base-api.ts'
+import { cardsSlice } from '@/services/cards/cards.slice.ts'
 import { decksSlice } from '@/services/decks/decks.slice.ts'
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     [decksSlice.name]: decksSlice.reducer,
+    [cardsSlice.name]: cardsSlice.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 })
